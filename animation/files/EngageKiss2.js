ab8b0969092641ced4e6fe445357bb69b75325e1
MUSESTAR-Cloud <xsrhub@163.com>
2022-07-12 18:11:53 +0800

    更新

--- animation/files/EngageKiss2.js
+++ animation/files/EngageKiss2.js
@@ -1,1 +1,2 @@
 document.write("<li><a href=\"https:\/\/musestar-silasila.github.io\/animation\/EngageKiss\/01.html\">第1集<\/a><\/li>");
+document.write("<li><a href=\"https:\/\/musestar-silasila.github.io\/animation\/EngageKiss\/02.html\">第2集<\/a><\/li>");
