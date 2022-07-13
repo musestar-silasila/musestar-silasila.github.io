40b5f7f2bc53b27fb01634eda7ee1a6669026a1c
MUSESTAR-Cloud <xsrhub@163.com>
2022-07-13 22:16:23 +0800

    更新

--- animation/files/jmdtypswdqny2.js
+++ animation/files/jmdtypswdqny2.js
@@ -1,1 +1,2 @@
 document.write("<li><a href=\"https:\/\/musestar-silasila.github.io\/animation\/jmdtypswdqny\/01.html\">第1集<\/a><\/li>");
+document.write("<li><a href=\"https:\/\/musestar-silasila.github.io\/animation\/jmdtypswdqny\/02.html\">第2集<\/a><\/li>");
