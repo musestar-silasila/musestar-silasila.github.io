29dee0440278c1220742fbaa02562153ac9396a3
MUSESTAR-Cloud <xsrhub@163.com>
2022-07-16 20:31:03 +0800

    更新

--- animation/files/bbjb2.js
+++ animation/files/bbjb2.js
@@ -1,1 +1,2 @@
 document.write("<li><a href=\"https:\/\/musestar-silasila.github.io\/animation\/bbjb\/01.html\">第1集<\/a><\/li>");
+document.write("<li><a href=\"https:\/\/musestar-silasila.github.io\/animation\/bbjb\/02.html\">第2集<\/a><\/li>");
