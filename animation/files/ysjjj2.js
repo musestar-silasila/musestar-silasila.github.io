70997746e706516312c50423dab941aaa8e9d72b
MUSESTAR-Cloud <xsrhub@163.com>
2022-07-25 16:26:55 +0800

    更新

--- animation/files/ysjjj2.js
+++ animation/files/ysjjj2.js
@@ -1,2 +1,3 @@
 document.write("<li><a href=\"https:\/\/musestar-silasila.github.io\/animation\/ysjjj\/01.html\">第1集<\/a><\/li>");
 document.write("<li><a href=\"https:\/\/musestar-silasila.github.io\/animation\/ysjjj\/02.html\">第2集<\/a><\/li>");
+document.write("<li><a href=\"https:\/\/musestar-silasila.github.io\/animation\/ysjjj\/03.html\">第3集<\/a><\/li>");
