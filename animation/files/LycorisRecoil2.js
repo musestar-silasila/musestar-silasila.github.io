e012711c9fe73583a19b9683c63f71bd45627224
MUSESTAR-Cloud <xsrhub@163.com>
2022-07-25 16:16:13 +0800

    更新

---
+++ animation/files/LycorisRecoil2.js
@@ -0,0 +1,4 @@
+document.write("<li><a href=\"https:\/\/musestar-silasila.github.io\/animation\/LycorisRecoil\/01.html\">第1集<\/a><\/li>");
+document.write("<li><a href=\"https:\/\/musestar-silasila.github.io\/animation\/LycorisRecoil\/02.html\">第2集<\/a><\/li>");
+document.write("<li><a href=\"https:\/\/musestar-silasila.github.io\/animation\/LycorisRecoil\/03.html\">第3集<\/a><\/li>");
+document.write("<li><a href=\"https:\/\/musestar-silasila.github.io\/animation\/LycorisRecoil\/04.html\">第4集<\/a><\/li>");
