b8247263986dd1062d60408d180bdf21e91048a3
MUSESTAR-Cloud <xsrhub@163.com>
2022-07-27 22:01:58 +0800

    更新

--- animation/files/jmdtypswdqny2.js
+++ animation/files/jmdtypswdqny2.js
@@ -1,3 +1,4 @@
 document.write("<li><a href=\"https:\/\/musestar-silasila.github.io\/animation\/jmdtypswdqny\/01.html\">第1集<\/a><\/li>");
 document.write("<li><a href=\"https:\/\/musestar-silasila.github.io\/animation\/jmdtypswdqny\/02.html\">第2集<\/a><\/li>");
 document.write("<li><a href=\"https:\/\/musestar-silasila.github.io\/animation\/jmdtypswdqny\/03.html\">第3集<\/a><\/li>");
+document.write("<li><a href=\"https:\/\/musestar-silasila.github.io\/animation\/jmdtypswdqny\/04.html\">第4集<\/a><\/li>");
